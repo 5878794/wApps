029077734336948d76491a644ced302ab20380ad
5878794@qq.com <xf771026aa>
2019-03-20 11:09:04 +0800

    add notSupport page

---
+++ jsProjectManagement/miniprogram/pages/notSupport/notSupport.js
@@ -0,0 +1,12 @@
+const regeneratorRuntime = require('../../js/regeneratorRuntime.js'),
+      app = require('../../js/wxApp');
+
+
+
+app.ready({
+    data:{},
+    onLoad:rs=>{
+        console.log('notSupport page')
+    }
+});
+
