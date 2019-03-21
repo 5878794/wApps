67c1acb38c91e863e7461079aad1425fdd6322e8
5878794@qq.com <xf771026aa>
2019-03-21 15:42:07 +0800

    index page 30%

--- jsProjectManagement/miniprogram/pages/index/index.js
+++ jsProjectManagement/miniprogram/pages/index/index.js
@@ -1,6 +1,7 @@
 const regeneratorRuntime = require('../../js/regeneratorRuntime.js'),
       wxApp = require('../../js/wxApp'),
-    app = getApp();
+	  canvasChat = require('../../template/day'),
+	  app = getApp();
 
 
 wxApp.ready({
@@ -23,8 +24,8 @@ wxApp.ready({
 		console.log(data)
 
 
-		this.createCanvas('canvas1',15);
-		this.createCanvas('canvas2',45);
+		canvasChat('canvas1',15);
+		canvasChat('canvas2',45);
 	},
 
 
@@ -35,43 +36,43 @@ wxApp.ready({
                 url: '../notSupport/notSupport',
             });
         }
-    },
-	createCanvas:function(id,per){
-    	let windowWidth = wxApp.device().windowWidth,
-		    canvasCenter = parseInt(windowWidth*150/750)/2,
-		    endDeg = per*Math.PI*2/100-Math.PI/2,
-		    startDeg = 0-Math.PI/2;
-
-
-
-		// 使用 wx.createContext 获取绘图上下文 context
-		const context = wx.createCanvasContext(id);
-
-		context.beginPath();
-		context.setStrokeStyle('#d5d5d5');
-		context.setLineWidth(2);
-		context.arc(canvasCenter,canvasCenter,canvasCenter*0.9,0,2*Math.PI);
-		context.stroke();
-
-		context.beginPath();
-		context.setLineWidth(2);
-		context.setStrokeStyle('#0000e8');
-		context.arc(canvasCenter,canvasCenter,canvasCenter*0.9,startDeg,endDeg);
-		context.stroke();
-
-		context.beginPath();
-		context.setLineWidth(1);
-		context.font = 'bold 16px Arial';
-		context.textAlign = 'center';
-		context.textBaseline = 'middle';
-		context.fillStyle = '#666';
-		context.fillText(per+"%", canvasCenter, canvasCenter);
-		context.stroke();
-
-		context.draw();
-
-
-	}
+    }
+	// createCanvas:function(id,per){
+    	// let windowWidth = wxApp.device().windowWidth,
+	// 	    canvasCenter = parseInt(windowWidth*150/750)/2,
+	// 	    endDeg = per*Math.PI*2/100-Math.PI/2,
+	// 	    startDeg = 0-Math.PI/2;
+	//
+	//
+	//
+	// 	// 使用 wx.createContext 获取绘图上下文 context
+	// 	const context = wx.createCanvasContext(id);
+	//
+	// 	context.beginPath();
+	// 	context.setStrokeStyle('#d5d5d5');
+	// 	context.setLineWidth(2);
+	// 	context.arc(canvasCenter,canvasCenter,canvasCenter*0.9,0,2*Math.PI);
+	// 	context.stroke();
+	//
+	// 	context.beginPath();
+	// 	context.setLineWidth(2);
+	// 	context.setStrokeStyle('#0000e8');
+	// 	context.arc(canvasCenter,canvasCenter,canvasCenter*0.9,startDeg,endDeg);
+	// 	context.stroke();
+	//
+	// 	context.beginPath();
+	// 	context.setLineWidth(1);
+	// 	context.font = 'bold 16px Arial';
+	// 	context.textAlign = 'center';
+	// 	context.textBaseline = 'middle';
+	// 	context.fillStyle = '#666';
+	// 	context.fillText(per+"%", canvasCenter, canvasCenter);
+	// 	context.stroke();
+	//
+	// 	context.draw();
+	//
+	//
+	// }
 
 
 });
