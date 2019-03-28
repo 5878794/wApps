a27cd0589d8f27a7328054d0bd0952c91c941d0f
5878794@qq.com <xf771026aa>
2019-03-28 14:10:26 +0800

    table page 50%

--- jsProjectManagement/miniprogram/pages/table/table.js
+++ jsProjectManagement/miniprogram/pages/table/table.js
@@ -6,9 +6,11 @@ const regeneratorRuntime = require('../../js/regeneratorRuntime.js'),
 
 wxApp.ready({
     data:{
-	    scrollRightWidth:'',
-	    scrollLeft:'',
-	    scrollTop:''
+	    titleTop:0,
+	    menuLeft:0,
+	    // scrollRightWidth:'',
+	    // scrollLeft:'',
+	    // scrollTop:''
     },
 	onLoad:function(){
         this.init().then(rs=>{
@@ -21,69 +23,29 @@ wxApp.ready({
 
 
     },
-	scroll:function(e){
-
-    	let left = e.detail.scrollLeft,
-		    top = e.detail.scrollTop;
-
-		this.setData({
-			scrollLeft:left,
-			scrollTop:top
-		});
-	},
 
 
 
 	init:async function(){
-		let device = wxApp.device(),
-			width = device.screenWidth;
+		setInterval(()=>{
+			this.setTopAndMenuStyle();
+		},1000)
 
-		let left = await wxApp.getDomParam('#table_main_left');
-		left = left.width;
+	},
+	onPageScroll:function(e){
+		console.log(13)
+		this.setTopAndMenuStyle();
+	},
+	setTopAndMenuStyle:async function(){
+		let scroll = await wxApp.getScrollState(),
+			scrollTop = scroll.top,
+			scrollLeft = scroll.left;
 
 		this.setData({
-			scrollRightWidth:width-left+'px'
+			titleTop:scrollTop+'px',
+			menuLeft:scrollLeft+'px'
 		});
-
 	}
-	// createCanvas:function(id,per){
-    	// let windowWidth = wxApp.device().windowWidth,
-	// 	    canvasCenter = parseInt(windowWidth*150/750)/2,
-	// 	    endDeg = per*Math.PI*2/100-Math.PI/2,
-	// 	    startDeg = 0-Math.PI/2;
-	//
-	//
-	//
-	// 	// 使用 wx.createContext 获取绘图上下文 context
-	// 	const context = wx.createCanvasContext(id);
-	//
-	// 	context.beginPath();
-	// 	context.setStrokeStyle('#d5d5d5');
-	// 	context.setLineWidth(2);
-	// 	context.arc(canvasCenter,canvasCenter,canvasCenter*0.9,0,2*Math.PI);
-	// 	context.stroke();
-	//
-	// 	context.beginPath();
-	// 	context.setLineWidth(2);
-	// 	context.setStrokeStyle('#0000e8');
-	// 	context.arc(canvasCenter,canvasCenter,canvasCenter*0.9,startDeg,endDeg);
-	// 	context.stroke();
-	//
-	// 	context.beginPath();
-	// 	context.setLineWidth(1);
-	// 	context.font = 'bold 16px Arial';
-	// 	context.textAlign = 'center';
-	// 	context.textBaseline = 'middle';
-	// 	context.fillStyle = '#666';
-	// 	context.fillText(per+"%", canvasCenter, canvasCenter);
-	// 	context.stroke();
-	//
-	// 	context.draw();
-	//
-	//
-	// }
-
-
 });
 
 //
